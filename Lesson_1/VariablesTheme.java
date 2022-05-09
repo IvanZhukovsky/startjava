c3319b96a5952bb07c17a425beaac4c169974105
IvanZhukovsky <izhukowsky@gmail.com>
2022-05-09 23:23:23 +0500

    ДЗ1

---
+++ Lesson_1/VariablesTheme.java
@@ -0,0 +1,26 @@
+public class VariablesTheme {
+    public static void main(String[] args) {
+
+        System.out.println("1. Создание переменных и вывод их значений на консоль");
+
+        byte memorySize = 8;
+        short processorFrequency = 2100;
+        long occupiedSpaceHDD = 102679347200l;
+        float totalSpaceHDD = 245.11f;
+        double freeSpaceHDD = 146.69;
+        char processorType = 'M';
+        boolean isOn = true;
+
+        System.out.println(memorySize);
+        System.out.println(processorFrequency);
+        System.out.println(occupiedSpaceHDD);
+        System.out.println(totalSpaceHDD);
+        System.out.println(freeSpaceHDD);
+        System.out.println(processorType);
+        System.out.println(isOn);
+        System.out.println();
+
+
+
+    }
+}
