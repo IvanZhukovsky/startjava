9c230e32aa139212da0c2533b056b29010a5f1e0
IvanZhukovsky_MacBookAir <izhukowsky@gmail.com>
2022-05-19 19:10:55 +0500

    мелкая правка

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,6 +1,5 @@
 public class Calculator {
     public static void main(String[] args) {
-        //+, -, *, /, ^, %
         char sign = '%';
         int number1 = 10;
         int number2 = 3;
@@ -17,7 +16,7 @@ public class Calculator {
         } else if (sign == '^') {
             result = number1;
             for (int i = 1; i < number2; i++) {
-                result = result*number1;
+                result = result * number1;
             }
         } else if (sign == '%') {
             result = number1 % number2;
