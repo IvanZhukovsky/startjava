f03e1189fbc1c7c63c61dd9a573d453d620e60e5
IvanZhukovsky_MacBookAir <izhukowsky@gmail.com>
2022-05-20 14:26:43 +0500

    ДЗ№1 по ООП готово

---
+++ Lesson_2/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+    String sex = "man";
+    String name = "Ivan";
+    int height = 176;
+    int weight = 85;
+    int age = 39;
+
+    void go() {
+    }
+
+    void seat() {
+    }
+
+    void run() {
+    }
+
+    void talk() {
+    }
+    
+    void learn() {
+    }
+}
