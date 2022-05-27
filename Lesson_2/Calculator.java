ce7d054e508c6690deecc5420c4a792207b0fd2e
IvanZhukovsky_MacBookAir <izhukowsky@gmail.com>
2022-05-27 12:34:19 +0500

    исправлено по замечаниям

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,46 +1,41 @@
 public class Calculator {
     
-        private char sign;
-        private int number1;
-        private int number2;
-        private int result;
+    private char sign;
+    private int number1;
+    private int number2;
+    private int result;
 
-        public void setSign(char sign) {
-            this.sign = sign;
-        }
+    public void setSign(char sign) {
+        this.sign = sign;
+    }
 
-        public void setNumber1(int number1) {
-            this.number1 = number1;
-        }
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
 
-        public void setNumber2(int number2) {
-            this.number2 = number2;
-        }
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
 
-        public int getResult() {
-            switch (sign) {
-                case ('+'):
-                    result = number1 + number2;
-                    break;
-                case ('-'):
-                    result = number1 - number2;
-                    break;
-                case ('*'):
-                    result = number1 * number2;
-                    break;
-                case ('/'):
-                    result = number1 / number2;
-                    break;
-                case ('^'):
-                    result = number1;
-                    for (int i = 1; i < number2; i++) {
-                        result = result * number1;
-                    }
-                    break;
-                case ('%'):
-                    result = number1 % number2;
-                    break;
-            }
-            return result;
+    public int calculate() {
+        switch (sign) {
+            case '+':
+                return number1 + number2;
+            case '-':
+                return number1 - number2;
+            case '*':
+                return number1 * number2;
+            case '/':
+                return number1 / number2;
+            case '^':
+                result = number1;
+                for (int i = 1; i < number2; i++) {
+                    result *= number1;
+                }
+                return result;
+            case '%':
+                return number1 % number2;
         }
+        return -1;
+    }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,6 +4,7 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
+        String choose;
 
         while (true) {
             System.out.print("Введите первое число: ");
@@ -12,9 +13,15 @@ public class CalculatorTest {
             calculator.setSign(scanner.next().charAt(0));
             System.out.print("Введите второе число: ");
             calculator.setNumber2(scanner.nextInt());
-            System.out.println(calculator.getResult());
-            System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-            if (scanner.next().equals("no")) {
+            System.out.println(calculator.calculate());
+            while (true) {
+                System.out.print("Хотите продолжить вычисления? [yes/no]: ");
+                choose = scanner.next();
+                if (choose.equals("no") || choose.equals("yes")) {
+                    break;
+                }
+            }
+            if (choose.equals("no")) {
                 break;
             }
         }
