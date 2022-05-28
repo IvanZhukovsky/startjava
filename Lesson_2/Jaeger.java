11b54e5fbfa0e037f8ad4fd78c63ef58a84b1a7a
IvanZhukovsky_MacBookAir <izhukowsky@gmail.com>
2022-05-28 17:29:43 +0500

    Вторая часть итогового ДЗ готова

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,117 @@
+public class Jaeger {
+    private String modelName;
+    private String mark;
+    private String status;
+    private String origin;
+    private float height;
+    private float weight;
+    private float speed;
+    private int strenght;
+    private int armor;
+
+    public Jaeger () {
+    }
+
+    public Jaeger (String modelName, String mark, String status, String origin,
+            float height, float weight, float speed, int strenght, int armor) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.status = status;
+        this.origin = origin;
+        this.height = height;
+        this.weight = weight;
+        this.speed = speed;
+        this.strenght = strenght;
+        this.armor = armor;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public int getStrenght() {
+        return strenght;
+    }
+
+    public void setStrenght(int strenght) {
+        this.strenght = strenght;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public boolean drift() {
+        return true;
+    }
+
+    public void move() {
+        System.out.println("moving");
+    }
+
+    public String scanKaiju() {
+        return "scanning";
+    }
+
+    public void useVortexCannon(){
+        System.out.println("activated");
+    }
+
+    
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,70 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        //Создаем первого робота
+        Jaeger jaeger1 = new Jaeger();
+        jaeger1.setModelName("Cherno Alpha");
+        jaeger1.setMark("Mark-1");
+        jaeger1.setStatus("Active");
+        jaeger1.setOrigin("Russia");
+        jaeger1.setHeight(85.34f);
+        jaeger1.setWeight(2.412f);
+        jaeger1.setSpeed(3f);
+        jaeger1.setStrenght(10);
+        jaeger1.setArmor(10);
+        //Выводим информацию о первом роботе
+        System.out.println("Робот " + jaeger1.getModelName() + " " + jaeger1.getMark());
+        System.out.println("В данный момент имеет статус: " + jaeger1.getStatus());
+        System.out.println("Страна происхождения робота: " + jaeger1.getOrigin());
+        System.out.println("Робот имеет следующие технические характеристики:");
+        System.out.println("Высота: " + jaeger1.getHeight() + " м");
+        System.out.println("Вес: " + jaeger1.getWeight() + " тн");
+        System.out.println("Скорость: " + jaeger1.getSpeed() + " м/мин");
+        System.out.println("Сила: " + jaeger1.getStrenght());
+        System.out.println("Броня: " + jaeger1.getArmor());
+        //Создаем второго робота
+        Jaeger jaeger2 = new Jaeger("Gipsy Danger", "Mark-3", "Active", "USA", 
+                79.25f, 1.98f, 7f, 8, 6);
+        //Выводим информацию о втором роботе
+        System.out.println("Робот " + jaeger2.getModelName() + " " + jaeger2.getMark());
+        System.out.println("В данный момент имеет статус: " + jaeger2.getStatus());
+        System.out.println("Страна происхождения робота: " + jaeger2.getOrigin());
+        System.out.println("Робот имеет следующие технические характеристики:");
+        System.out.println("Высота: " + jaeger2.getHeight() + " м");
+        System.out.println("Вес: " + jaeger2.getWeight() + " тн");
+        System.out.println("Скорость: " + jaeger2.getSpeed() + " м/мин");
+        System.out.println("Сила: " + jaeger2.getStrenght());
+        System.out.println("Броня: " + jaeger2.getArmor());
+        //Подготовка к бою
+        System.out.println("В бой отправлется " + jaeger1.getModelName());
+        if (jaeger1.drift()) {
+            jaeger1.move();
+            System.out.println(jaeger1.scanKaiju());
+            jaeger1.useVortexCannon();
+        }
+        System.out.println("В бой отправлется " + jaeger2.getModelName());
+        if (jaeger2.drift()) {
+            jaeger2.move();
+            System.out.println(jaeger2.scanKaiju());
+            jaeger2.useVortexCannon();
+        }
+        //Бой
+        System.out.println(jaeger1.getModelName() + " получает удар!");
+        System.out.println(jaeger1.getModelName() 
+                + " теряет 1 очко брони и теряет 1 очко скорости");
+        jaeger1.setArmor(jaeger1.getArmor() - 1);
+        jaeger1.setSpeed(jaeger1.getSpeed() - 1f);
+        System.out.println(jaeger2.getModelName() + " получает сильный удар!");
+        System.out.println(jaeger2.getModelName() + " уничтожен!");
+        jaeger2.setStatus("Destroyed");
+        //Оценка состояния роботов после боя
+        System.out.println("Оценка состояния роботов после боя.");
+        System.out.println("Робот " + jaeger1.getModelName() + " " + jaeger1.getMark());
+        System.out.println("В данный момент имеет статус: " + jaeger1.getStatus());
+        System.out.println("Робот имеет следующие технические характеристики:");
+        System.out.println("Скорость: " + jaeger1.getSpeed() + " м/мин");
+        System.out.println("Сила: " + jaeger1.getStrenght());
+        System.out.println("Броня: " + jaeger1.getArmor());
+        System.out.println("Робот " + jaeger2.getModelName() + " " + jaeger2.getMark());
+        System.out.println("В данный момент имеет статус: " + jaeger2.getStatus());
+    }
+}
